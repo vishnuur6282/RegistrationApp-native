19c6404b49b2b13b14dc0051daa76137f4708782
Vishnu U R <vishnu.ur@experionglobal.local>
2023-12-22 10:50:06 +0530

    minor formatting appjs

--- App.tsx
+++ App.tsx
@@ -1,12 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 
 import LoginScreen from './src/screens/login/index';
